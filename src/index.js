9d3a1b16309ee8be0bbf6539fea44bb843a927b3
rhfiguer <rhfiguer@gmail.com>
2021-06-04 14:22:19 +0200

    Partial Env config

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//--------------------------------------------------------
+// We import
+//--------------------------------------------------------
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import proyectoReact from './components/proyectoReact';
+
+//--------------------------------------------------------
+// We render
+//--------------------------------------------------------
+
+ReactDOM.render(<proyectoReact/>, document.getElementById('proyecto'));
